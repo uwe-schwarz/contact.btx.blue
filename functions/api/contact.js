394cfacdedd69b5906a01574b3595e0f89043a09
Uwe Schwarz <git@iq42.de>
2022-08-07 15:58:32 +0200

    try to send mail

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -5,9 +5,47 @@ export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
     let pretty = JSON.stringify([...input], null, 2);
-    return new Response(pretty, {
+
+    let content = "";
+    for( var i of input ) {
+      content += i[0] + ": " + i[1] + "\n";
+    }
+
+    let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+        },
+        "body": JSON.stringify({
+            "personalizations": [
+                { "to": [ {"email": "e38383@icloud.com",
+                        "name": "Carina Schwarz"}]}
+            ],
+            "from": {
+                "email": "contact@carinaschwarz.dog",
+                "name": "Kontaktformular",
+            },
+            "subject": "Test Subject",
+            "content": [{
+                "type": "text/plain",
+                "value": "Test message content\n\n" + content,
+            }],
+        }),
+    });
+
+    let respContent = "";
+    // only send the mail on "POST", to avoid spiders, etc.
+    if( request.method == "POST" ) {
+        const resp = await fetch(send_request);
+        const respText = await resp.text();
+
+        respContent = resp.status + " " + resp.statusText + "\n\n" + respText;
+
+    }
+
+    return new Response(respContent, {
       headers: {
-        'Content-Type': 'application/json;charset=utf-8',
+        'Content-Type': 'text/plain',
       },
     });
   } catch (err) {
