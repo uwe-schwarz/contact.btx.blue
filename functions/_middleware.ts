776bfd7b9758064c3679ec8f66f9f73eb9365414
Uwe Schwarz <git@iq42.de>
2022-08-10 21:00:36 +0200

    env

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,11 +1,11 @@
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest: PagesFunction = (context) => mailChannelsPlugin({
+export const onRequest: PagesFunction = ({env}) => mailChannelsPlugin({
   personalizations: [
     {
       to: [{ name: "Kontaktformular", email: "uwe@idle.btx.blue" }],
       reply_to: {
-        name: context.request.formData.get('name'),
+        name: "reply_to",
         email: "mail@example.org",
       },
       headers: { "x-test": "foobar" },
@@ -15,7 +15,7 @@ export const onRequest: PagesFunction = (context) => mailChannelsPlugin({
     name: "ACME Support",
     email: "formular@carinaschwarz.dog",
   },
-  subject: context.env.DKIM_PRIVATE_KEY,
+  subject: env.DKIM_PRIVATE_KEY,
   respondWith: () => {
     return new Response(null, {
       status: 302,
@@ -24,4 +24,4 @@ export const onRequest: PagesFunction = (context) => mailChannelsPlugin({
       },
     });
   },
-})(context);
+});
