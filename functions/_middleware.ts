6deb02914e89ebcb6850ec5a8ff558250f73abb8
Uwe Schwarz <git@iq42.de>
2022-08-10 20:48:02 +0200

    headers

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -4,15 +4,16 @@ export const onRequest: PagesFunction = (context) => mailChannelsPlugin({
   personalizations: [
     {
       to: [{ name: "Kontaktformular", email: "uwe@idle.btx.blue" }],
+      reply_to: {
+        name: context.env.DKIM_PRIVATE_KEY,
+        email: "mail@example.org",
+      },
+      headers: { "x-test": "foobar" },
     },
   ],
   from: {
     name: "ACME Support",
     email: "formular@carinaschwarz.dog",
-  },
-  reply_to: {
-    name: context.env.DKIM_PRIVATE_KEY,
-    email: "mail@example.org",
   },
   respondWith: () => {
     return new Response(null, {
