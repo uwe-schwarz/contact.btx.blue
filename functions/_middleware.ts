7aa97cebc9c519eb57f5fa54858613912322e689
Uwe Schwarz <git@iq42.de>
2022-08-10 20:40:56 +0200

    reply_to

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,7 +3,7 @@ import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 export const onRequest: PagesFunction = (context) => mailChannelsPlugin({
   personalizations: [
     {
-      to: [{ name: "Some User", email: "user@cloudflare.com" }],
+      to: [{ name: "Some User", email: "uwe@idle.btx.blue" }],
       "dkim_domain": "example.com", // The value has to be the domain you added DKIM records to and where you're sending your email from
       "dkim_selector": "mailchannels",
       "dkim_private_key": context.env.DKIM_PRIVATE_KEY
@@ -12,6 +12,10 @@ export const onRequest: PagesFunction = (context) => mailChannelsPlugin({
   from: {
     name: "ACME Support",
     email: "support@example.com",
+  },
+  reply_to: {
+    name: "reply",
+    email: "mail@example.org",
   },
   respondWith: () => {
     return new Response(
