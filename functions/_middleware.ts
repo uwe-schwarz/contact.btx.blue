e621a7b1fa4964a41a1af947c73520764b4d9986
Uwe Schwarz <git@iq42.de>
2022-08-10 21:10:30 +0200

    env again

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -11,7 +11,7 @@ export const onRequest: PagesFunction = (context) => mailChannelsPlugin({
     name: "ACME Support",
     email: "formular@carinaschwarz.dog",
   },
-  subject: context.formData.get('name'),
+  subject: context.env.DKIM_PRIVATE_KEY,
   respondWith: () => {
     return new Response(null, {
       status: 302,
